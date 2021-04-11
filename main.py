4e0b1b5dfbfbd2bcfd7777eeb2a429b99904f6a4
Scaffus <scaffus.scaffus@gmail.com>
2021-04-11 19:18:21 +0200

    modified main.py to save the file as a mp3

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # This whole code is my "take" of the tutorial of NeuralNine: https://www.youtube.com/watch?v=9G47uuBTz04 (this man is <3<3 btw)
 
-import pyttsx3
-import PyPDF2
-import os
+from pyttsx3 import init
+from PyPDF2 import PdfFileReader
+from os import listdir
 
 def Audi0Book():
 
     try:
-        books = os.listdir(path='books/')
+        books = listdir(path='books/')
 
         i = 0
 
@@ -32,19 +32,22 @@ def Audi0Book():
 
         try:
             with open('books/{}.pdf'.format(book), 'rb') as desired_book:
-                
-                reader = PyPDF2.PdfFileReader(desired_book)
 
-                audio_reader = pyttsx3.init()
+                full_text = ''
+                
+                reader = PdfFileReader(desired_book)
+
+                audio_reader = init()
                 audio_reader.setProperty('rate', 150)
 
                 for page in range(reader.numPages):
                     
                     next_page = reader.getPage(page)
                     content = next_page.extractText()
+                    full_text += content
 
-                    audio_reader.say(content)
-                    audio_reader.runAndWait()
+                audio_reader.save_to_file(full_text, "Audio book of {}.mp3".format(book))
+                audio_reader.runAndWait()
 
         except FileNotFoundError:
             print('No book find with title \"{}\", you do not have to add the .pdf extension.'.format(book))
